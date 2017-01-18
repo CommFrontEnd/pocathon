fe211cadf2f7486b25f85254e2e54a98875014dd
Guillaume SARRADIN <gsarradin@sii.fr>
2017-01-18 15:46:23 +0100

    accountView

--- src/app/components/AccountsView.vue
+++ src/app/components/AccountsView.vue
@@ -4,33 +4,36 @@
             <div class="c-field__label">N° de compte:</div>
             <!-- //'<div class="c-field__value" v-text="account.accountNum
             " />' + -->
-            <div class="c-field__value" />{{ account.accountNum }}</div>
-    </div>
-    <div class="c-list-item__actions fa fa-angle-right"></div>
-    </a>
-    <div class="c-list-item__content js-account-details">
-        <div class="c-field">
-            <div class="c-field__label"></div>
-            <div class="c-field__value">{{ account.accountSolde }}} <span class="down fa fa-angle-down"></span></div>
-        </div>
-        <div class="c-list-item__detail">
-            <div class="c-field" v-for="operation in account.lastOperations">
-                <div class="c-field__label">{{ operation.label }}</div>
-                <div class="c-field__value is-valid">{{ operation.amount }} €</div>
+            <div class="c-field__value">{{ account.accountNum }}</div>
+            <div class="c-list-item__actions fa fa-angle-right"></div>
+        </a>
+        <div class="c-list-item__content js-account-details">
+            <div class="c-field">
+                <div class="c-field__label"></div>
+                <div class="c-field__value">{{ account.accountSolde }}} <span class="down fa fa-angle-down"></span></div>
+            </div>
+            <div class="c-list-item__detail">
+                <div class="c-field" v-for="operation in account.lastOperations">
+                    <div class="c-field__label">{{ operation.label }}</div>
+                    <div class="c-field__value is-valid">{{ operation.amount }} €</div>
+                </div>
             </div>
         </div>
+    </div>
 </template>
 
 
-<script type="text/javascript">
-const Accounts = { 
-  template: 
+<script>
+export default {
+    name: 'accountsView',
+    props: [],
 
-  data: { 
+
+data() {
+    return {
     accounts: [ 
       {
-      accountNum
-      : 424242,
+      accountNum: 424242,
       accountSolde: 15042,
       lastOperations: [
         {
@@ -49,5 +52,16 @@ const Accounts = {
       }
     ]
   }
+},
+
+    ready: function() {
+      this.fetchData();
+    },
+
+    methods: {
+        fetchData : function() { 
+          // this.$http.get('http://', function(data) {}, function(error) {})
+        }
+    }
 }
 </script>
