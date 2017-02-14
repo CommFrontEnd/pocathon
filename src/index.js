c2cd7ac39857d5109aee53de3274ca4dd5add9d7
Erwan Baccon <erwan.baccon@gmail.com>
2017-02-14 11:03:42 +0100

    - Sécurité des accès mis en place : Si la session n'a pas été initialisé alors la redirection se fait sur le login

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Account from './components/account/Account';
 // Rooter
 import { createHistory } from 'history';
 import { Router, Route, useRouterHistory } from 'react-router';
+//import { Router, Route, browserHistory } from 'react-router'
 
 import './main.css';
 
@@ -19,11 +20,21 @@ const browserHistory = useRouterHistory(createHistory)({
     basename: '/poc/react'
 });
 
+
+function requireAuth(nextState, replace) {
+	if (!sessionStorage.getItem('user')) {
+		replace({
+			pathname: '/login',
+			state: { nextPathname: nextState.location.pathname }
+		})
+	}
+}
+
 const Root = () => {
 	return (
 		<Router history={browserHistory}>
 			<Route path="/login" component={Login} />
-			<Route path="/" component={App}>
+			<Route path="/" component={App} onEnter={requireAuth}>
 				<Route path="accounts" component={Accounts} />
 				<Route path="accounts/:accountId" component={Account} />
 			</Route>
@@ -31,6 +42,7 @@ const Root = () => {
 		</Router>
 	)
 }
+
 
 render(
   <Root />,
